4f7f109b7eac2ac074559d3ee86f45392d7c3a3d
Olaf Kock <olaf.kock@liferay.com>
2017-11-26 22:31:08 +0100

    removed 6.1 files, updated version

--- docroot/WEB-INF/src/de/olafkock/liferay/navigation/CustomNavigationKeys.java
+++
@@ -1,8 +0,0 @@
-package de.olafkock.liferay.navigation;
-
-public class CustomNavigationKeys {
-
-	public static final String PUBLIC_GROUP_IDS  = "customNavigationSitesPublic";
-	public static final String PRIVATE_GROUP_IDS = "customNavigationSitesPrivate";
-
-}
